ee97dadf050670ab0eeeb4ad6916b46e8e3cb029
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-12-18 06:38:12 +0800

    Add home page

--- app/src/main/java/com/example/purrmodoro/MainActivity.java
+++ app/src/main/java/com/example/purrmodoro/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.purrmodoro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnStartFocus = findViewById(R.id.btnStartFocus);
+        Button btnShortBreak = findViewById(R.id.btnShortBreak);
+        Button btnLongBreak = findViewById(R.id.btnLongBreak);
+        Button btnViewProgress = findViewById(R.id.btnViewProgress);
+
+        ImageView homeCatImage = findViewById(R.id.homeCatImage);
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.cat_studying) // Use the underscore version
+                .into(homeCatImage);
+
+        btnStartFocus.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, TimerActivity.class);
+            startActivity(intent);
+        });
+
+        btnShortBreak.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ShortBreakActivity.class);
+            startActivity(intent);
+        });
+
+        btnLongBreak.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LongBreakActivity.class);
+            startActivity(intent);
+        });
+
+        btnViewProgress.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProgressActivity.class);
+            startActivity(intent);
         });
     }
 }
